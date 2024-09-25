73c0f8da7c9fe8c1085d51a69ed15430ea11e9a9
Abbie Graff <acgraff@cps.edu>
2024-09-25 10:58:04 -0500

    began #2

--- a2.py
+++ a2.py
@@ -33,10 +33,17 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
         # If you get stuck on this one, we encourage you to attempt the other conditions
         #   and come back to this one afterwards
 
+        elif pattern[pind] == "%":
+             if pind == len(pattern)-1:
+                  source.append(source[sind]) #questionable on this line 
         # 3) if we reached the end of the source but not the pattern
         elif sind == len(source) and pind < len(pattern):
              return None
         # 4) if the current thing in the pattern is an _
+        elif pattern[pind] == "_":
+             result.append(source[sind])
+             pind += 1
+             sind += 1 
 
         # 5) if the current thing in the pattern is the same as the current thing in the
         # source
